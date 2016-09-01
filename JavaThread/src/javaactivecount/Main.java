102d2ebddc04b9e8d843c9f71289f8481ed9a91a
Arx.1971 <arx.1971@gmail.com>
2016-09-01 16:19:34 -0400

    updated

---
+++ JavaThread/src/javaactivecount/Main.java
@@ -0,0 +1,16 @@
+package javaactivecount;
+
+public class Main {
+	
+	public static void main(String args[]){
+		
+		Thread [] threads = new Thread[Thread.activeCount()];
+		
+		int n = Thread.enumerate(threads);
+		
+		for(int i=0; i<n; i++){
+			System.out.println(threads[i].toString());
+		}
+		
+	}
+}
